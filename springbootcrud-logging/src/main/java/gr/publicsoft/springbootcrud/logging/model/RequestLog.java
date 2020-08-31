8e7dac9e8a41b879f90197932a5493b131f51361
antwpapak <antwpapak@gmail.com>
2020-08-31 20:11:33 +0300

    Add lombok annotations

--- springbootcrud-logging/src/main/java/gr/publicsoft/springbootcrud/logging/model/RequestLog.java
+++ springbootcrud-logging/src/main/java/gr/publicsoft/springbootcrud/logging/model/RequestLog.java
@@ -1,8 +1,17 @@
 package gr.publicsoft.springbootcrud.logging.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
 /**
  * A simple POJO to represent a request log entry
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
 public class RequestLog {
 
     private String method;
--- springbootcrud-logging/src/main/java/gr/publicsoft/springbootcrud/logging/model/ResponseLog.java
+++ springbootcrud-logging/src/main/java/gr/publicsoft/springbootcrud/logging/model/ResponseLog.java
@@ -1,8 +1,17 @@
 package gr.publicsoft.springbootcrud.logging.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * A simple POJO to represent a response log entry
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
 public class ResponseLog {
 
     private int statusCode;
