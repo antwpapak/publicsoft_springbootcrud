61d941798f9391e1c69a2ce63c5283aab2b9e2a4
antwpapak <antwpapak@gmail.com>
2020-08-31 16:43:33 +0300

    Remove deprecated RepositoryRestConfigurerAdapter

--- springbootcrud-webapp/src/main/java/gr/publicsoft/springbootcrud/config/RepositoryConfig.java
+++ springbootcrud-webapp/src/main/java/gr/publicsoft/springbootcrud/config/RepositoryConfig.java
@@ -3,10 +3,11 @@ package gr.publicsoft.springbootcrud.config;
 import gr.publicsoft.springbootcrud.model.Person;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
 @Configuration
-public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
+public class RepositoryConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(Person.class);
